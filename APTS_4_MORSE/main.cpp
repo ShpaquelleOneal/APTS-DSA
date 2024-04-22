d1e734694c3fe657d60d1de341560da248f50480
ShpaquelleOneal <109593992+ShpaquelleOneal@users.noreply.github.com>
2024-04-22 17:55:13 +0300

    Task 4 - initial

---
+++ APTS_4_MORSE/main.cpp
@@ -0,0 +1,13 @@
+#include <fstream>
+
+int main() {
+    // Open input and output files
+    std::ifstream inputFile("server.in");
+    std::ofstream outputFile("server.out");
+
+
+    // Close input and output files
+    inputFile.close();
+    outputFile.close();
+    return 0;
+}
